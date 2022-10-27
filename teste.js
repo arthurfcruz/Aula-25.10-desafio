d09cede522ff745c0907b5beb01a58df56448e3d
Arthur <arthurfelixcruz@gmail.com>
2022-10-27 20:06:50 -0300

    criando funcao login e cadastro

--- teste.js
+++ teste.js
@@ -1,12 +1,38 @@
 var nome = []
 var senha = []
 var fazer
+var index = 0
 
 function solicitarNome(){
-    nome = prompt("Qual seu nome?")
-    senha = prompt ("Digite sua senha")
+    nome[index] = prompt("Qual seu nome?")
+    senha[index] = prompt ("Digite sua senha")
+    index++
 }
 
 function oqFazer(){
     fazer = prompt("o que deseja faze? 1-cadastrar; 2-fazer login; 3-excluir um cadastro; 4-encerrar o programa")
+    if (fazer == 1) {
+        solicitarNome()
+    }
+    if (fazer == 2) {
+        fazerLogin()
+    }
+    if (fazer == 3){
+        excluirCadastro()
+    }
+    if (fazer == 4) {
+        encerrarPrograma()
+    }
+}
+
+function fazerLogin() {
+    
+}
+
+function excluirCadastro() {
+    
+}
+
+function encerrarPrograma() {
+    
 }
