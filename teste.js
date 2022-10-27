6a179c9fae74b4bec21796932e4453e8085d71a2
Arthur <arthurfelixcruz@gmail.com>
2022-10-26 21:46:20 -0300

    adicionando a funcao fazer

---
+++ teste.js
@@ -0,0 +1,12 @@
+var nome = []
+var senha = []
+var fazer
+
+function solicitarNome(){
+    nome = prompt("Qual seu nome?")
+    senha = prompt ("Digite sua senha")
+}
+
+function oqFazer(){
+    fazer = prompt("o que deseja faze? 1-cadastrar; 2-fazer login; 3-excluir um cadastro; 4-encerrar o programa")
+}
